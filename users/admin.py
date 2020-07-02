268775811d2ea1220fd91ba6b0c088a1d25af616
김지수 <Jisu_Kim@gimjisuui-iMac.local>
2020-07-02 16:19:00 +0900

    Extend Django Admin Pannel

--- users/admin.py
+++ users/admin.py
@@ -1,22 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
-
-
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-
-    """ Custom User Admin """
-
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = (
-        "language",
-        "currency",
-        "superhost",
-    )
 
 
 # class CustomUserAdmin(admin.ModelAdmin):
 #     pass
 
-
 # admin.site.register(models.User, CustomUserAdmin)
+# "same as below"
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
