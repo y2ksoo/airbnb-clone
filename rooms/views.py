465e416630dbe19265e6d6024584d768365d039f
김지수 <Jisu_Kim@gimjisuui-iMac.local>
2020-07-07 16:32:07 +0900

    Using Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,12 @@
-from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page = int(page or 1)
-    page_size = 5
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/all_rooms.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 5)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/all_rooms.html", {"rooms": rooms})
+
