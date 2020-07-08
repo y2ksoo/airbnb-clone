a644ace6048974e4f2f0d372cfb29b2e9f4ba3e0
김지수 <Jisu_Kim@gimjisuui-iMac.local>
2020-07-08 17:23:13 +0900

    Urls &404 handling

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView
+from django.urls import reverse
+from django.http import Http404
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -10,4 +13,13 @@ class HomeView(ListView):
     paginate_by = 5
     paginate_orphans = 2
     ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404
 
