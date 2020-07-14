2978d188142e14bb9afe5fae14e1f43180d20d4b
김지수 <Jisu_Kim@gimjisuui-iMac.local>
2020-07-14 13:38:41 +0900

    signup

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
+    path("signup", views.SignupView.as_view(), name="signup"),
 ]
